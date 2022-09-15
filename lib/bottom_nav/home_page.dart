6a6fb17fa7a13711416e2313d8332ce46d964529
shahriar-swe <shahriarswe@gmail.com>
2022-09-15 17:10:44 +0600

    1. create two list view (Category and all news)

--- lib/bottom_nav/home_page.dart
+++ lib/bottom_nav/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/category_model/model_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,54 +25,196 @@ class _HomePageState extends State<HomePage> {
   ];
 
 
+
+  final categoryItem = Categories.category_list;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Card(
-            elevation: 6,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Row(
-                  children: [
-                    Icon(Icons.search),
-                    Text("Search"),
-                  ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                elevation: 6,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Row(
+                      children: [
+                        Icon(Icons.search),
+                        Text("Search"),
+                      ],
+                    ),
+                  ),
+                  height: 40,
+                  width: double.infinity,
                 ),
               ),
-              height: 40,
-              width: double.infinity,
-            ),
-          ),
-          SizedBox(height: 10,),
-          SizedBox(
-            height: 200.0,
-            width: double.infinity,
-            child: Carousel(
-              dotSpacing: 15.0,
-              dotSize: 6.0,
-              dotIncreasedColor: Colors.black,
-              dotBgColor: Colors.transparent,
-              indicatorBgPadding: 10.0,
-              dotPosition: DotPosition.bottomCenter,
-              images: images
-                  .map((item) => Container(
-                child: Image.network(
-                  item,
-                  fit: BoxFit.cover,
+
+
+
+              SizedBox(height: 5,),
+              Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.grey,
                 ),
-              ))
-                  .toList(),
-            ),
+              ),
+
+
+              //create carousel slider
+              SizedBox(height: 10,),
+              SizedBox(
+                height: 150.0,
+                width: double.infinity,
+                child: Carousel(
+                  dotSpacing: 15.0,
+                  dotSize: 6.0,
+                  dotIncreasedColor: Colors.black,
+                  dotBgColor: Colors.transparent,
+                  indicatorBgPadding: 10.0,
+                  dotPosition: DotPosition.bottomCenter,
+                  images: images
+                      .map((item) => Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          item,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ))
+                      .toList(),
+                ),
+              ),
+              //create carousel slider
+
+
+
+
+              SizedBox(height: 10,),
+              Text("Categories",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),),
+
+
+
+              SizedBox(height: 10,),
+              Container(
+                height: 120,
+                //width: double.infinity,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categoryItem.length,
+                    itemBuilder: (context,index){
+                      var category = categoryItem[index];
+                      return InkWell(
+                        /*onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => PreventionPage(
+                            preventionsDetails: prevention,
+                          )));
+                        },*/
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.blue[100],
+                                ),
+
+
+                                child: Image.asset(category.categoryImg,height: 70,width: 70,fit: BoxFit.cover,),
+                              ),
+                            ),
+
+
+                            Text(category.categoryName,style: TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.w600),),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+
+
+
+              SizedBox(height: 5,),
+              Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.grey,
+                ),
+              ),
+
+
+
+              SizedBox(height: 10,),
+              Text("All News",style: TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold),),
+
+
+
+              SizedBox(height: 10,),
+              Container(
+                height: 180,
+                //width: double.infinity,
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                  //physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true, // This is important ⬇️
+                    itemCount: categoryItem.length,
+                    itemBuilder: (context,index){
+                      var category = categoryItem[index];
+                      return InkWell(
+                        /*onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => PreventionPage(
+                            preventionsDetails: prevention,
+                          )));
+                        },*/
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 120,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.blue[100],
+                              ),
+
+
+                              child: Image.asset(category.categoryImg,height: 70,width: 70,fit: BoxFit.cover,),
+                            ),
+
+
+                            Text(category.categoryName,style: TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.w600),),
+                            Text(category.categoryName,style: TextStyle(color: Colors.black,fontSize: 12,fontWeight: FontWeight.w600),),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
